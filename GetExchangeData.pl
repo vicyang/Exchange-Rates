19a7b7c14307b1a98af7150282b4d39fc0ec892e
vicyang <523066680@163.com>
2017-10-12 22:29:39 +0800

    Signed-off-by: vicyang <523066680@163.com>

--- GetExchangeData.pl
+++ GetExchangeData.pl
@@ -8,7 +8,7 @@
 use strict;
 use Encode;
 use Time::Local;
-use LWP::Simple;
+use File::Slurp;
 use LWP::UserAgent;
 use Data::Dumper qw/Dumper/;
 use IO::Handle;
@@ -19,12 +19,12 @@ our $di = -1;
 our @all;
 
 our $FH;
-open $FH, ">:raw:crlf", "history.txt" or die "$!";
+open $FH, ">:raw", "history.txt" or die "$!";
 $FH->autoflush(1);
 
 our $ua = LWP::UserAgent->new();
 
-my $from = time_to_date(time() - 24*3600*5);
+my $from = time_to_date(time() - 24*3600*1);
 my $to   = time_to_date(time());               # today
 
 my $i = 1;
@@ -35,13 +35,7 @@ my $res;
 while (1)
 {
     print "Getting Page: $i\n";
-    @all=();
-    # $res = $ua->get(
-    #         "http://srh.bankofchina.com/search/whpj/search.jsp?".
-    #         "erectDate=2017-10-01&nothing=2017-10-10&pjname=1316".
-    #         "&page=1"
-    #         );
-
+    @all = ();
     $res = $ua->post(
             "http://srh.bankofchina.com/search/whpj/search.jsp",
             [
@@ -52,20 +46,16 @@ while (1)
             ]
         );
 
-    printf $FH Dumper $res;
-    last;
+    $res->content() =~/var m_nCurrPage = (\d+)/;
+    $curpg = $1;
+    last if ($curpg == $prvpg);    #页面并不会因为页码超出范围而404，超出后会指向有效的最后一页
+                                   #如果返回页码和上次一致，判定为结束
 
-    
-    # $stream =~/var m_nCurrPage = (\d+)/;
-    # $curpg = $1;
-    # last if ($curpg == $prvpg);    #页面并不会因为页码超出范围而404，超出后会指向有效的最后一页
-    #                                #如果返回页码和上次一致，判定为结束
-    
-    # @all=split('\n', $stream);
-    # get_info();
+    @all = split('\n', $res->content() );
+    get_info();
 
-    # $i++;
-    # $prvpg = $curpg;
+    $i++;
+    $prvpg = $curpg;
 }
 
 close $FH;
@@ -76,41 +66,44 @@ sub get_info
 {
     our @all;
     our $FH;
-
     my ($i, @arr);
 
-    $i=0;
-    for (@all) 
+    $i = 0;
+    for my $e (@all)
     {
-        if (/th>(.*)<\/th>/) 
+        if ($e =~/th>(.*)<\/th>/ )
         {
-            $arr[$i++] = encode('gbk',$1);
+            $arr[$i++] = decode('utf8', $1);
         }
     }
 
     #获取美元信息
-    my $spec = decode('utf-8',"<td>美元<\/td>");
+    my $spec = "<td>美元<\/td>";
     my $begin = 0;
     my $j = 0;
 
-    foreach (0..$#all) 
+    for my $idx ( 0 .. $#all )
     {
-        if ($all[$_]=~/$spec/i) 
+        #printf encode 'gbk', $all[$idx];
+        if ( $all[$idx] =~/$spec/i )
         {
-            $begin=1;
-            print $FH "\n";
+            $begin = 1;
+            print $FH "\r\n";
         }
-        if ($begin==1 and $j < $i )
+
+        if ( $begin == 1 and $j < $i )
         {
-            next if ($all[$_]=~/^\s+$/); #如果是空行
-            $all[$_]=~/td>(.*)<\/td>/i;
-            print $FH encode('utf8', $1), "\t";
+            #略过空行
+            next if ( $all[$idx]=~/^\s+$/ );
+            $all[$idx] =~/td>(.*)<\/td>/i;
+            print $FH $1, "\t";
             $j++;
-        } 
-        if ($all[$_]=~/<\/tr>/)   #末尾重置
+        }
+
+        if ($all[$idx]=~/<\/tr>/)   #末尾重置
         {
-            $j=0;
-            $begin=0;
+            $j = 0;
+            $begin = 0;
         }
     }
 }
