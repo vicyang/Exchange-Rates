92e6549695df44cd300365f22991b887bff513e5
vicyang <523066680@163.com>
2017-10-12 20:21:31 +0800

    精简代码

--- GetExchangeData.pl
+++ GetExchangeData.pl
@@ -2,7 +2,7 @@
     获取中行外汇牌价-美元栏目的信息
     Auth: 523066680
     Date: 2017-10
-    https://github.com/vicyang/Exchange-Rate
+    https://github.com/vicyang/Exchange-Rates
 =cut
 
 use Encode;
@@ -14,51 +14,30 @@ use HTML::TableExtract;
 use IO::Handle;
 STDOUT->autoflush(1);
 
-our $data = [];
-our $di = -1;
-our @all;
-
+our $URL = "http://srh.bankofchina.com/search/whpj/search.jsp";
 our $FH;
 open $FH, ">:raw", "history.txt" or die "$!";
-$FH->autoflush(1);
 
 our $ua = LWP::UserAgent->new( 
-                timeout => 5,
-                keep_alive => 1, 
-                agent => 'Mozilla/5.0',
+            timeout => 5, keep_alive => 1, agent => 'Mozilla/5.0',
           );
 
 my $from = time_to_date(time() - 24*3600*1);
 my $to   = time_to_date(time());               # today
 
 my $pageid = 1;
-my $curr;
-my $prev = -1;
-my $res;
-my $s;
+my $content;
 
 while (1)
 {
     print "Getting Page: $pageid\n";
-    @all = ();
+    $content = get_page( $from, $to, $pageid );
 
-    $res = $ua->post(
-            "http://srh.bankofchina.com/search/whpj/search.jsp",
-            [
-                erectDate => $from,
-                nothing   => $to,
-                pjname    => "1316",
-                page      => $pageid
-            ]
-        );
+    #页码超出后会指向有效的最后一页而非404，实际页码不同步时结束循环
+    $content =~/var m_nCurrPage = (\d+)/;
+    last if ( $1 != $pageid );
 
-    #页码超出后会指向有效的最后一页，若页码和上次一致，判定为结束
-    $res->content() =~/var m_nCurrPage = (\d+)/;
-    $curr = $1;
-    last if ($curr == $prev);
-
-    get_info( $res->content() );
-    $prev = $curr;
+    get_info( $content );
     $pageid++;
 }
 
@@ -69,22 +48,37 @@ printf("Done\n");
 sub get_info 
 {
     my $html_str = shift;
+    
     # count => 1 表示选择第二个表格。
     my $obj = HTML::TableExtract->new( depth => 0, count => 1 );
     $obj->parse($html_str);
 
     my $table;
-    my $date;
     grep { $table = $_ } $obj->tables;
 
     for my $row ( $table->rows )
     {
-        next if ( $row->[1] eq "" or (not $row->[2] =~/\d/) );
-        print encode('gbk', decode('utf8', shift @$row )), "\t";
-        $date = pop @$row; 
-        grep { printf "%.2f\t", $_ } @$row;
-        print "$date\n";
+        next if ( $row->[1] eq '' );  #表格最末一行为空
+        grep { print encode('gbk', decode('utf8', $_)), "\t" } @$row;
+        print "\n";
     }
+}
+
+sub get_page
+{
+    our $ua;
+    my ($from, $to, $pageid) = @_;
+    my $res;
+    $res = $ua->post( 
+        $URL,
+        [
+            erectDate => $from,
+            nothing   => $to,
+            pjname    => "1316",
+            page      => $pageid
+        ]
+    );
+    return $res->content();
 }
 
 sub time_to_date
