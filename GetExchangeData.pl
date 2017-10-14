69119b57aef40ada42cd24a5ebb337636ee68268
vicyang <523066680@163.com>
2017-10-14 19:39:19 +0800

    通过参数调用获取数据 参数格式>script.pl YY-MM-DD YY-MM-DD name.perldb

--- GetExchangeData.pl
+++ GetExchangeData.pl
@@ -8,6 +8,7 @@
 use Encode;
 use threads;
 use threads::shared;
+use Try::Tiny;
 use Time::HiRes qw/sleep/;
 use Time::Local;
 use File::Slurp;
@@ -18,21 +19,24 @@ use HTML::TableExtract;
 
 use IO::Handle;
 STDOUT->autoflush(1);
-$Data::Dumper::Indent = 1;
-$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Indent = 0;
+#$Data::Dumper::Sortkeys = 1;
 
 our $URL = "http://srh.bankofchina.com/search/whpj/search.jsp";
 our $ua = LWP::UserAgent->new( 
             timeout => 5, keep_alive => 1, agent => 'Mozilla/5.0',
           );
 
-our %hash :shared;
+our $hash :shared;
 our @task :shared;
 $hash = shared_clone( {} );
 
-my $file = "hash.perldb";
-my $from = time_to_date(time() - 24*3600*5);
-my $to   = time_to_date(time());
+if ( check_arguments() == 0 ) { printf "Wrong format\n"; exit; }
+my ($from, $to, $file) = @ARGV;
+
+# my $file = "2015.perldb";
+# my $from = "2015-01-01";
+# my $to   = "2016-01-01";
 
 my $pageid = 1;
 my @ths;
@@ -47,7 +51,15 @@ while ( threads->list( threads::running ) )
 #分离线程
 grep { $_->detach() } @ths;
 
-write_file( $file, { binmode => ":raw" }, Dumper \%hash );
+
+printf("Dumping ... ");
+my $dbstr = Dumper($hash);
+$dbstr =~s/(\],)/$1\r\n  /g;
+$dbstr =~s/(\},)/$1\r\n/g;
+$dbstr =~s/(\=\> \{)/$1\r\n  /g;
+$dbstr =~s/(\= \{)/$1\r\n/g;
+
+write_file( $file, { binmode => ":raw" }, $dbstr );
 printf("Done\n");
 
 sub func
@@ -64,7 +76,7 @@ sub func
         $content =~/var m_nCurrPage = (\d+)/;
         last if ( $1 != $task[$idx] );
 
-        $timestamp = get_info( $content );
+        $timestamp = get_exchange_data( $content );
         printf "[%d] mission: %4d time: %s\n", 
                 $idx, $task[$idx], $timestamp;
         #归零
@@ -72,32 +84,29 @@ sub func
     }
 }
 
-sub get_info 
+sub get_exchange_data
 {
-    our %hash;
-    my $html_str = shift;
+    my ( $html_str ) = @_;
+    my ($obj, $table, $timestamp, $date, $time);
 
-    # count => 1 表示选择第二个表格。
-    my $obj = HTML::TableExtract->new( depth => 0, count => 1 );
+    #count => 1 表示选择第二个表格
+    $obj = HTML::TableExtract->new( depth => 0, count => 1 );
     $obj->parse($html_str);
 
-    my $table;
     grep { $table = $_ } $obj->tables;
 
-    my $timestamp;
-    for my $row ( $table->rows )
+    for my $ele ( $table->rows )
     {
-=info
-        去掉第一行抬头
-        去掉第一列货币类型
-        表格最后一行为空
-=cut
-        shift @$row;
-        next if ( $row->[1] eq '' );
-        next if ( not $row->[1] =~/\d/ );
+        shift @$ele;                       '去掉第一行抬头';
+        next if ( $ele->[1] eq '' );       '去掉第一列货币类型';
+        next if ( not $ele->[1] =~/\d/ );  '表格最后一行为空';
 
-        $timestamp = pop @$row;
-        $hash{ $timestamp } = shared_clone([ @$row ]);
+        $timestamp = pop @$ele;
+        $timestamp =~/^(.{10}) (.{8})/;
+        ($date, $time) = ($1, $2);
+        if (not exists $hash->{$date}) { $hash->{$date} = shared_clone( {} ) }
+        $hash->{$date}{$time} = shared_clone( [@$ele] );
+        #push @{$hash->{$date}}, shared_clone({$timestamp, join("\t", @$row)});
     }
 
     return $timestamp;
@@ -128,3 +137,23 @@ sub time_to_date
     return sprintf "%d-%02d-%02d", $year,$mon,$day;
 }
 
+sub check_arguments
+{
+    if ( $#ARGV < 2 ) { printf "Too few arguments\n"; exit; }
+    my ( $from, $to, $file) = @ARGV;
+    my $today = time_to_date( time() );
+    my $res = 1;
+    for my $dt ( $from, $to )
+    {
+        if ( $dt =~/(\d{4})-(\d{2})-(\d{2})/ ) 
+        {
+            $res = 0 if ( $dt gt $today );
+            $res = 0 if ( $1 lt 2007 );
+            try   { timelocal(0,0,0, $3, $2-1, $1-1900) } 
+            catch { $res = 0 };
+        }
+    }
+
+    $res = 0 if ( $from gt $to );
+    return $res;
+}
