74929518f4936085d1cbc9f6cfc87414ec901d71
vicyang <523066680@163.com>
2017-10-18 22:03:06 +0800

    Signed-off-by: vicyang <523066680@163.com>

--- Visualization/display.pl
+++ Visualization/display.pl
@@ -12,6 +12,7 @@ use Time::HiRes qw/sleep/;
 use Time::Local;
 use File::Slurp;
 use Data::Dumper;
+use List::Util qw/sum min max/;
 
 use IO::Handle;
 use OpenGL qw/ :all /;
@@ -19,12 +20,28 @@ use OpenGL::Config;
 use feature 'state';
 
 STDOUT->autoflush(1);
-printf("loading...");
-our $hash = eval read_file( "../Data/2015.perldb" );
-our @days = (keys %$hash);
-printf("Done.\n");
 
-our $WinID;
+BEGIN
+{
+    our $WinID;
+
+    printf("loading...");
+    our $hash = eval read_file( "../Data/2017.perldb" );
+    our @days = (sort keys %$hash);
+    our ($MIN, $MAX) = (1000.0, 0.0);
+    for my $d (@days)
+    {
+        for my $t ( keys %{$hash->{$d}} )
+        {
+            if ($hash->{$d}{$t}[0] < $MIN) { $MIN = $hash->{$d}{$t}[0] }
+            if ($hash->{$d}{$t}[0] > $MAX) { $MAX = $hash->{$d}{$t}[0] }
+        }
+    }
+
+    printf("Done.\n");
+    printf("min: %.3f, max: %.3f\n", $MIN, $MAX );
+    
+}
 
 =struct
     $hash = {
@@ -41,30 +58,42 @@ sub display
     state @times;
     state $day;
     state $i = 0;
-    our ($hash, @days);
-    my $hour;
+    state ($min, $max);
+    our ($hash, @days, $MIN);
+    my $hour, $time;
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glColor3f(0.8, 0.8, 0.8);
     #glRectf(0.0, 0.0, 100.0, 100.0);
 
-    if ( $i % 100 == 0 )
+    if ( $i % 2 == 0 )
     {
         $day = shift @days;
-        @times = keys %{ $hash->{$day} };
+        unless ( defined $day ) {
+            glutDestroyWindow( $WinID );
+            exit;
+        }
+
+        #时间清零，避免受到上一次影响
+        @times = ();
+        @rates = ();
+        #时间排序
+        @times = sort keys %{ $hash->{$day} };
+        @rates = map { $hash->{$day}{$_}[0] } @times;
+        ($min, $max) = ( min(@rates), max(@rates) );
+        printf("%s, count: %d, avg: %.3f\n", $day, $#rates+1, sum(@rates)/($#rates+1) );
     }
 
     $i++;
 
     glColor3f(1.0, 0.0, 0.0);
-    glBegin(GL_POINTS);
+    glBegin(GL_LINE_STRIP);
     grep 
     {
-        /^(\d+)/;
-        $hour = $1;
-        glVertex3f($hour*10.0, ($hash->{$day}{$_}[0] - 620.0)*10.0, 0.0);
+        /^(\d+):(\d+)/;
+        $time = ($1.$2)/8.0 - 200.0;
+        glVertex3f($time, ($hash->{$day}{$_}[0]-$MIN)*10.0 , 0.0);
         # print $hash->{$day}->{$_}->[0],"\n";
-        #print $_,"\n";
     }
     @times;
 
@@ -75,7 +104,7 @@ sub display
 
 sub idle 
 {
-    sleep 0.01;
+    sleep 0.02;
     glutPostRedisplay();
 }
 
@@ -83,11 +112,11 @@ sub init
 {
     glClearColor(0.0, 0.0, 0.0, 0.5);
     glPointSize(1.0);
-    glLineWidth(1.0);
+    glLineWidth(0.5);
     glEnable(GL_BLEND);
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_POINT_SMOOTH);
-    glEnable(GL_LINE_SMOOTH);
+    # glEnable(GL_POINT_SMOOTH);
+    # glEnable(GL_LINE_SMOOTH);
 }
 
 sub reshape 
@@ -117,10 +146,10 @@ sub hitkey
 sub main 
 {
     glutInit();
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE |GLUT_DEPTH |GLUT_MULTISAMPLE );
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE |GLUT_DEPTH  );
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(1,1);
-    our $WinID = glutCreateWindow("bunny");
+    our $WinID = glutCreateWindow("Display");
     &init();
     glutDisplayFunc(\&display);
     glutReshapeFunc(\&reshape);
