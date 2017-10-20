6c906b871811d1a8b9a4a1858af92a1cdaa68ce1
vicyang <523066680@163.com>
2017-10-20 09:21:19 +0800

    Signed-off-by: vicyang <523066680@163.com>

--- Visualization/practice/data_divide.pl
+++ Visualization/practice/data_divide.pl
@@ -1,5 +1,5 @@
 =info
-    时间段平摊
+    数据平摊
     Auth: 523066680
     Date: 2017-10
     https://github.com/vicyang/Exchange-Rates
@@ -18,30 +18,5 @@ printf("loading...");
 $hash = eval read_file( "./data.perldb" );
 printf("done\n");
 
-our $ref;
-my $min;
-my $curr;
-my $prev;
-
-grep 
-{
-    /^0?(\d+):0?(\d+)/;
-    $min = $1*60+$2;
-
-    $curr = $hash->{$_}[0];
-
-    printf "%s %03d %.2f\n", substr($_, 0, 5), $min, $hash->{$_}[0];
-
-    $prev = $curr;
-}
-sort keys %$hash;
-
-my $data;
-my $m_last = 24*60; #minutes
-for (my $m = 0; $m < $m_last; $m+= 10)
-{
-    $data->{$m} = 1;
-    #print "$m\n";
-}
 
 
