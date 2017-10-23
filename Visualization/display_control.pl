3d67f5c7a06d8a26d1e3c89d008067269dbd6764
vicyang <523066680@163.com>
2017-10-23 10:19:00 +0800

    Signed-off-by: vicyang <523066680@163.com>

--- Visualization/display_control.pl
+++ Visualization/display_control.pl
@@ -31,7 +31,7 @@ BEGIN
     our ($rx, $ry, $rz, $zoom) = (0.0, 0.0, 0.0, 1.0);
     our ($mx, $my, $mz) = (0.0, 0.0, 0.0);
 
-    our $DB_File = "../Data/2014.perldb.bin";
+    our $DB_File = "../Data/2017.perldb.bin";
     our $hash = retrieve( $DB_File );
     our @days = (sort keys %$hash);
     our $begin = 0;                  #展示数据的起始索引
