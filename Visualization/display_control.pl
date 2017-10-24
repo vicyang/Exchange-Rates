c41f2afd37134cd083e65e88bacf0724b508c95f
vicyang <523066680@163.com>
2017-10-24 11:08:13 +0800

    Signed-off-by: vicyang <523066680@163.com>

--- Visualization/display_control.pl
+++ Visualization/display_control.pl
@@ -100,10 +100,14 @@ INIT
         push @color_idx, { 'R' => 0.0, 'G' => 0.0, 'B' => 0.0 };
     }
 
-    fill_color( 20, 60, 1.0, 0.0, 0.0);
-    fill_color(100,100, 1.0, 0.6, 0.0);
-    fill_color(200,100, 0.2, 0.8, 0.2);
-    fill_color(300,300, 0.2, 0.6, 1.0);
+    # fill_color( 20, 60, 1.0, 0.3, 0.3);
+    # fill_color(100,100, 1.0, 0.6, 0.0);
+    # fill_color(200,100, 0.2, 0.8, 0.2);
+    # fill_color(300,300, 0.2, 0.6, 1.0);
+
+    fill_color( 20,200, 1.0, 0.6, 0.2);
+    fill_color(150,200, 0.3, 1.0, 0.3);
+    fill_color(280,300, 0.2, 0.5, 1.0);
 
     sub fill_color 
     {
@@ -279,7 +283,6 @@ sub init
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_POINT_SMOOTH);
     glEnable(GL_LINE_SMOOTH);
-    glEnable(GL_LINE_STIPPLE);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     $tobj = gluNewTess();
